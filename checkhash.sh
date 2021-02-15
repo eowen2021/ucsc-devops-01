7a5c28d509a6fa7854d5b6ac62af0eb4e0c74707
Ed Owen <eowen@data-time.com>
2021-02-14 17:14:57 -0800

    changes from shfmt

--- checkhash.sh
+++ checkhash.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ -f ~/.hushlogin ]
-then
-    echo "File exists, All Good"
+if [ -f ~/.hushlogin ]; then
+	echo "File exists, All Good"
 else
-    touch ~/.hushlogin
-    echo "File created"
+	touch ~/.hushlogin
+	echo "File created"
 fi
